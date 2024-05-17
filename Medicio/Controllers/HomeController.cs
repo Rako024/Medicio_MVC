454bf77969265602afca9cc815b179c16baa6eb6
Rashid <residbabayev42@gmail.com>
2024-05-17 16:53:06 +0400

    17.05.2024 Medicio pushed

---
+++ Medicio/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Medicio.Controllers
+{
+    public class HomeController : Controller
+    {
+        UserManager<AppUser> _userManager;
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+    }
+}
